e8e9b431df3c4b1bcbe708a9218a12a6e4033591
Jorge Luis <Jorge Luis@jorge-Desktop>
2013-10-20 16:14:40 -0400

    Mejoré la panalla de inicio. Proximo commit: Botón de iniciar para buscar parqueo.

--- src/com/example/secunet/InicioActivity.java
+++ src/com/example/secunet/InicioActivity.java
@@ -1,17 +1,30 @@
 package com.example.secunet;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.PendingIntent;
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
+import android.nfc.NdefMessage;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import org.ksoap2.SoapEnvelope;
 import org.ksoap2.serialization.SoapObject;
@@ -22,6 +35,9 @@ import com.parse.Parse;
 import com.parse.ParseInstallation;
 import com.parse.PushService;
 
+import org.ndeftools.*;
+import org.ndeftools.externaltype.AndroidApplicationRecord;
+
 import java.util.List;
 
 public class InicioActivity extends Activity {
@@ -30,12 +46,21 @@ public class InicioActivity extends Activity {
     Intent intentMain;
     Intent intentCheck;
     WifiManager wifiManager;
+    private Button entrar;
     List<WifiConfiguration> listaWifi;
     ProgressDialog Cargando;
+    NfcAdapter nfcAdapter;
+    PendingIntent nfcPendingIntent;
+    AlertDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
-    	 Parse.initialize(this,"NJE50gi9UOxCggYxSO2gVFyMkNVQy0w14mZNdcFI","iMZgZ2mzfCJMw8wlyuhqNy89gDFkf6KVtqmyaCgF");
+    	entrar = (Button) findViewById(R.id.btBuscar);
+    	
+    	nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        nfcPendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, this.getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+        
+    	Parse.initialize(this,"NJE50gi9UOxCggYxSO2gVFyMkNVQy0w14mZNdcFI","iMZgZ2mzfCJMw8wlyuhqNy89gDFkf6KVtqmyaCgF");
     	super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio1);
         PushService.setDefaultPushCallback(InicioActivity.this, InicioActivity.class);
@@ -43,34 +68,52 @@ public class InicioActivity extends Activity {
         PushService.subscribe(InicioActivity.this,"global", MainActivity.class);
         ParseInstallation.getCurrentInstallation().saveInBackground();
         wifiManager = (WifiManager)getSystemService(Context.WIFI_SERVICE);
-        Cargando = ProgressDialog.show(InicioActivity.this, "Cargando", "Espere por favor...");
 
-        /*
-        if(!wifiManager.isWifiEnabled()){
-            wifiManager.setWifiEnabled(true);
-            while(!wifiManager.isWifiEnabled()){
-
+        getMacAddress();
+        
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setNegativeButton("Ok", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+            	dialog.dismiss();
             }
-            //wifiManager.disconnect();
-        }
-
-        if (WiFiExiste()){
-            conectarWiFi();
-        }else{
-            agregarWiFi();
-            conectarWiFi();
-       }
-
-        while (!isConnected(InicioActivity.this)) {
-
-        }*/
+        });
+        builder.setPositiveButton("Iniciar", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+//                InicioActivity.this.startActivity(intentMain);
+//                finish();
+            }
+        });
+        
+        builder.setMessage("Toca el botón de buscar parqueos para iniciar!").setTitle("Hola!");
+        dialog = builder.create();
+        
+        Cargando = ProgressDialog.show(InicioActivity.this, "Cargando", "Espere por favor...");
 
         intentMain = new Intent(InicioActivity.this, MainActivity.class);
         intentCheck = new Intent(InicioActivity.this, CheckActivity.class);
-        getMacAddress();
+        
+//        entrar.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                InicioActivity.this.startActivity(intentMain);
+//                finish();
+//            }
+//        });
         new checkParqueado().execute();
     }
-
+    
+    @Override
+    public void onPause() {
+        super.onPause();
+        disableForegroundMode();
+    }
+    
+    @Override
+    public void onResume(){
+        super.onResume();
+        enableForegroundMode();
+    }
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -78,6 +121,82 @@ public class InicioActivity extends Activity {
         return true;
     }
 
+    @Override
+    public void onNewIntent(Intent intent) { // this method is called when an NFC tag is scanned
+
+        // check for NFC related actions
+        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(intent.getAction())) {   
+        	dialog.dismiss();
+            Toast.makeText(this, "Tag Encontrado", Toast.LENGTH_SHORT).show();
+        	
+            Parcelable[] messages = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+            Tag elTag = (Tag) intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+
+            TextView textView = (TextView) findViewById(R.id.leido);
+            textView.setText(bytesToHexString(elTag.getId()));
+            
+            if (messages != null) { 
+                //Log.d(TAG, "Found " + messages.length + " NDEF messages");
+
+                //vibrate(); // signal found messages :-)
+
+                // parse to records
+                for (int i = 0; i < messages.length; i++) {
+                    try {
+                        List<Record> records = new Message((NdefMessage)messages[i]);
+
+                        //Log.d(TAG, "Found " + records.size() + " records in message " + i);
+
+                        for(int k = 0; k < records.size(); k++) {
+                            //Log.d(TAG, " Record #" + k + " is of class " + records.get(k).getClass().getSimpleName());
+                            
+                            Record record = records.get(k);
+                            if(record instanceof AndroidApplicationRecord) {
+                                AndroidApplicationRecord aar = (AndroidApplicationRecord) record;
+                                TextView textView1 = (TextView) findViewById(R.id.idTag);
+                                textView1.setText( "El Paquete es " + aar.getPackageName());
+                                //Log.d(TAG, "Package is " + aar.getPackageName());
+                            }
+                        }
+                    } catch (Exception e) {
+                        Toast.makeText(this, "Problema parseando el mensaje", Toast.LENGTH_SHORT).show(); //Log.e(TAG, "Problem parsing message", e);
+                        
+                    }
+                }
+            }
+        } else {
+            // ignore
+        }        
+    }
+    
+    private String bytesToHexString(byte[] src) {
+        StringBuilder stringBuilder = new StringBuilder("0x");
+        if (src == null || src.length <= 0) {
+            return null;
+        }
+
+        char[] buffer = new char[2];
+        for (int i = 0; i < src.length; i++) {
+            buffer[0] = Character.forDigit((src[i] >>> 4) & 0x0F, 16);  
+            buffer[1] = Character.forDigit(src[i] & 0x0F, 16);  
+            System.out.println(buffer);
+            stringBuilder.append(buffer);
+        }
+
+        return stringBuilder.toString();
+    }
+    
+    public void enableForegroundMode() {
+        // foreground mode gives the current active application priority for reading scanned tags
+        IntentFilter tagDetected = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED); // filter for tags
+        IntentFilter[] writeTagFilters = new IntentFilter[] {tagDetected};
+        nfcAdapter.enableForegroundDispatch(this, nfcPendingIntent, writeTagFilters, null);
+    }
+
+	public void disableForegroundMode() {
+	    nfcAdapter.disableForegroundDispatch(this);
+	}
+    
     public Boolean isConnected(Context context) {
         ConnectivityManager connectivityManager = (ConnectivityManager)
                 context.getSystemService(Context.CONNECTIVITY_SERVICE);
@@ -161,8 +280,7 @@ public class InicioActivity extends Activity {
                 finish();
             }
             else{
-                InicioActivity.this.startActivity(intentMain);
-                finish();
+                dialog.show();
             }
         }
     }
